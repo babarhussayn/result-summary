d4eed68f6e67e472edcfb31f120fd8ec00843114
babarhussain <babarhusain262@gmail.com>
2024-10-22 17:26:24 +0500

    main

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -3,7 +3,13 @@ import React from "react";
 const HomePage = () => {
   return (
     <>
-      <div className="flex-1">HomePage</div>
+      <div className="flex-1">
+        <div className="w-full flex justify-center items-center ">
+          <div className="">
+            <div className="flex ">card</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
